f361276149cedd7c0efc98d002e6a5c36f9f9df7
BOYOUNG-KANG <28287@naver.com>
2023-06-06 02:57:48 +0900

    [feat] jpql vs querydsl

---
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,37 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import study.querydsl.test.Hello;
+import study.querydsl.test.QHello;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@Transactional
+@SpringBootTest
+class QuerydslApplicationTests {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = new QHello("h");
+
+        Hello result = query
+                .selectFrom(qHello)
+                .fetchOne();
+
+        assertThat(result).isEqualTo(hello);
+        assertThat(result.getId()).isEqualTo(hello.getId());
+    }
+}
